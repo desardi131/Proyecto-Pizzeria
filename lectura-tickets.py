718c03c55510ae3f640fe30e3b8f05185538b0dc
desardi131 <desardi131@gmail.com>
2024-06-05 16:35:51 +0200

    V0.0.3 lectura-tickets.py iniciacion

---
+++ lectura-tickets.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+class Ticket():
+
+    def __init__(self) -> None:
+        self.nombre = nombre
+        self.telefono = telefono
+        self.hora = hora
+        self.precio = precio
+        self.contenido = contenido
+        
