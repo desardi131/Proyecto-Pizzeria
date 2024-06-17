6522ae39dc5d0835f50a19caf9e3ba28d7148f10
desardi131 <desardi131@gmail.com>
2024-06-17 18:48:36 +0200

    V0.0.9 Test tesseract con ticket de pizzeria y añadido .gitignore

--- Practica-lectura.py
+++ Practica-lectura.py
@@ -1,2 +1,11 @@
 from PIL import Image
 import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\desardi131\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+image_path = './Ticket-prueba.jpg'
+
+text = pytesseract.image_to_string(Image.open(image_path))
+
+print('Texto extraido: ')
+print(text)
