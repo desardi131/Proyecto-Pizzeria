8fbf2c35db07e7c9ccfcd6efd7e998d8f0f204fa
desardi131 <desardi131@gmail.com>
2024-06-28 19:07:31 +0200

    V0.0.11 reinstalacion de opencv-python y test

--- Practica-captura.py
+++ Practica-captura.py
@@ -1,15 +1,15 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) # funcion para capturar
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
+    ret, frame = cap.read() 
+    if not ret: # Si la camara no graba rada, el script se para
         break
 
-    cv2.imshow('Captura de imagen', frame)
+    cv2.imshow('Captura de imagen', frame) # muestra la ventana que graba la camara
 
-    if cv2.waitKey(1) & 0xFF == ord('s'):
+    if cv2.waitKey(1) & 0xFF == ord('s'): # Si presionamos la tecla 's' crea una captura y se cierra el programa
         cv2.imwrite('captura.jpg', frame)
         break
 
--- Practica-lectura.py
+++ Practica-lectura.py
@@ -3,7 +3,7 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\desardi131\AppData\Local\Programs\Tesseract-OCR\tesseract.exe' # Colocar siempre para usar tesseract
 
-image_path = './Ticket-prueba.jpg'
+image_path = './captura.jpg'
 
 text = pytesseract.image_to_string(Image.open(image_path),lang='spa')
 
